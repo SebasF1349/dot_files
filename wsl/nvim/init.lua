3114fa427766c6d1597970277ea16db269646968
SebasF1349 <ratablanca711@gmail.com>
2023-09-02 20:38:58 -0300

    bg transparency

--- wsl/nvim/init.lua
+++ wsl/nvim/init.lua
@@ -138,8 +138,18 @@ require('lazy').setup({
     priority = 1000,
     config = function()
       vim.cmd.colorscheme 'onedark'
+      -- Transparent Background
+      local groups = {
+        'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier', 'Statement', 'PreProc', 'Type',
+        'Underlined', 'Todo', 'String', 'Function', 'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr',
+        'NonText', 'SignColumn', 'CursorLineNr', 'EndOfBuffer',
+      }
+      for _, group in ipairs(groups) do
+        vim.api.nvim_set_hl(0, group, { bg = "none" })
+      end
     end,
   },
+
 
   {
     -- Set lualine as statusline
