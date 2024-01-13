05e25f437cb3c8e381cdeb848444ba1f95232673
SebasF1349 <ratablanca711@gmail.com>
2024-01-13 10:16:58 -0300

    change sections lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,12 +1,45 @@
 return {
-  -- Set lualine as statusline
-  -- See `:help lualine.txt`
   "nvim-lualine/lualine.nvim",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
     options = {
+      icons_enabled = true,
       theme = "catppuccin",
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
+      disabled_filetypes = {
+        statusline = {},
+        winbar = {},
+      },
+      ignore_focus = {},
+      always_divide_middle = true,
+      globalstatus = false,
+      refresh = {
+        statusline = 1000,
+        tabline = 1000,
+        winbar = 1000,
+      },
     },
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch", "diff", "diagnostics" },
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = { { "filetype", icon_only = true } },
+      lualine_z = { { "filename", path = 1 } },
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = { { "filename", path = 1 } },
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {},
   },
 }
