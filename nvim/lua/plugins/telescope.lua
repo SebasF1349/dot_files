528d1cc8b8944c17c11e36459722f021054e0e03
SebasF1349 <ratablanca711@gmail.com>
2024-01-13 11:25:22 -0300

    telescope flex + remove showcase

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -37,6 +37,7 @@ return {
             ["jk"] = actions.close,
           },
         },
+        layout_strategy = "flex",
       },
     })
 
@@ -81,13 +82,7 @@ return {
     -- See `:help telescope.builtin`
     vim.keymap.set("n", "<leader>?", builtin.oldfiles, { desc = "[?] Find recently opened files" })
     vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-    vim.keymap.set("n", "<leader>/", function()
-      -- You can pass additional configuration to telescope to change theme, layout, etc.
-      builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-        winblend = 10,
-        previewer = false,
-      }))
-    end, { desc = "[/] Fuzzily search in current buffer" })
+    vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find, { desc = "[/] Fuzzily search in current buffer" })
 
     local function telescope_live_grep_open_files()
       builtin.live_grep({
