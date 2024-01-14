5342fb5bb7bbd332c096337f122054776b1a9228
SebasF1349 <ratablanca711@gmail.com>
2024-01-14 01:40:28 -0300

    add autocmds

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,14 +4,3 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 require("core")
-
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
-vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = "*",
-})
