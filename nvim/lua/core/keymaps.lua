3e51018bcc244176d6e32b96377391bc7f96809e
SebasF1349 <ratablanca711@gmail.com>
2024-01-17 18:42:32 -0300

    fix paste keymap on visual mode

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -63,7 +63,7 @@ vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 
 -- Do things without affecting the registers
-vim.keymap.set("v", "p", '"_dp')
+vim.keymap.set("v", "p", '"_dP')
 vim.keymap.set("n", "x", '"_x')
 vim.keymap.set("n", "<Leader>p", '"0p')
 vim.keymap.set("n", "<Leader>P", '"0P')
