6fc812318a585ef6e6d1048c8fa0edd1405944ea
SebasF1349 <ratablanca711@gmail.com>
2024-02-07 18:44:32 -0300

    remove unnecessary keymaps

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -117,11 +117,6 @@ vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 
 -- Do things without affecting the registers
-vim.keymap.set("v", "p", '"_dP')
-vim.keymap.set("n", "x", '"_x')
-vim.keymap.set("n", "<Leader>p", '"0p')
-vim.keymap.set("n", "<Leader>P", '"0P')
-vim.keymap.set("v", "<Leader>p", '"0p')
 vim.keymap.set("n", "<Leader>c", '"_c')
 vim.keymap.set("n", "<Leader>C", '"_C')
 vim.keymap.set("v", "<Leader>c", '"_c')
