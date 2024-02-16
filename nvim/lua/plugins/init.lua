841c593a6668bd14db445060952beb2787245f9e
SebasF1349 <ratablanca711@gmail.com>
2024-02-16 10:01:13 -0300

    feat: add flash + remove eyeliner

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -10,13 +10,6 @@ return {
   -- Detect tabstop and shiftwidth automatically
   "tpope/vim-sleuth",
 
-  -- shows first unique letter of each word
-  {
-    "jinh0/eyeliner.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    opts = { highlight_on_key = true },
-  },
-
   -- "gc" to comment visual regions/lines
   {
     "numToStr/Comment.nvim",
