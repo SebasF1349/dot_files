f4c57110eb9e5ec0f9d3c04451477929d780cdde
SebasF1349 <ratablanca711@gmail.com>
2024-02-26 19:38:13 -0300

    feat: major update of lsp based on kickstart update - be sure it works

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,58 +1,64 @@
 local servers = {
   rust_analyzer = {
-    ["rust-analyzer"] = {
-      diagnostics = {
-        enable = true,
-      },
-      inlayHints = {
-        enable = true,
-        showParameterNames = true,
-        parameterHintsPrefix = "<- ",
-        otherHintsPrefix = "=> ",
-      },
-      imports = {
-        granularity = {
-          group = "module",
-        },
-        prefix = "self",
-      },
-      cargo = {
-        allFeatures = true,
-        buildScripts = {
+    settings = {
+      ["rust-analyzer"] = {
+        diagnostics = {
           enable = true,
         },
-      },
-      procMacro = {
-        enable = true,
-        ignored = {
-          ["async-trait"] = { "async_trait" },
-          ["napi-derive"] = { "napi" },
-          ["async-recursion"] = { "async_recursion" },
+        inlayHints = {
+          enable = true,
+          showParameterNames = true,
+          parameterHintsPrefix = "<- ",
+          otherHintsPrefix = "=> ",
         },
-      },
-      command = {
-        "cargo",
-        "clippy",
-      },
-      checkOnSave = {
-        command = "clippy",
+        imports = {
+          granularity = {
+            group = "module",
+          },
+          prefix = "self",
+        },
+        cargo = {
+          allFeatures = true,
+          buildScripts = {
+            enable = true,
+          },
+        },
+        procMacro = {
+          enable = true,
+          ignored = {
+            ["async-trait"] = { "async_trait" },
+            ["napi-derive"] = { "napi" },
+            ["async-recursion"] = { "async_recursion" },
+          },
+        },
+        command = {
+          "cargo",
+          "clippy",
+        },
+        checkOnSave = {
+          command = "clippy",
+        },
       },
     },
   },
+
   tsserver = {},
+
   html = {
     filetypes = { "html", "twig", "hbs" },
-    opts = {
-      settings = {
-        html = {
-          format = {
-            templating = true,
-            wrapLineLength = 120,
-            wrapAttributes = "auto",
-          },
-          hover = {
-            documentation = true,
-            references = true,
+    settings = {
+      opts = {
+        settings = {
+          html = {
+            format = {
+              templating = true,
+              wrapLineLength = 120,
+              wrapAttributes = "auto",
+            },
+            hover = {
+              documentation = true,
+              references = true,
+            },
           },
         },
       },
@@ -60,26 +66,36 @@ local servers = {
   },
 
   lua_ls = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
+    settings = {
+      Lua = {
+        workspace = { checkThirdParty = false },
+        telemetry = { enable = false },
+      },
     },
   },
 
   svelte = {
-    ["sveltejs/language-tools"] = {},
+    settings = {
+      ["sveltejs/language-tools"] = {},
+    },
   },
 
   cssls = {
-    ["css-lsp"] = {},
+    settings = {
+      ["css-lsp"] = {},
+    },
   },
 
   tailwindcss = {
-    ["tailwindcss-language-server"] = {},
+    settings = {
+      ["tailwindcss-language-server"] = {},
+    },
   },
 
   emmet_ls = {
-    filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+    settings = {
+      filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+    },
   },
 }
 
@@ -122,6 +138,7 @@ return {
       },
       "folke/neodev.nvim",
       "nvim-telescope/telescope.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
     },
     config = function()
       -- add border to the floating windows
@@ -138,82 +155,94 @@ return {
           end
         end,
       })
-      -- used to enable autocompletion (assign to every lsp server config)
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      local cmp_nvim_lsp = require("cmp_nvim_lsp")
-      capabilities = cmp_nvim_lsp.default_capabilities()
 
       local on_attach = function(data, bufnr)
         vim.lsp.inlay_hint.enable(bufnr, true)
-        local nmap = function(keys, func, desc)
-          if desc then
-            desc = "LSP: " .. desc
-          end
 
-          vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+        local nmap = function(keys, func, desc)
+          vim.keymap.set("n", keys, func, { buffer = bufnr, desc = "LSP:" .. desc })
         end
 
         nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
         nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-
-        nmap("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-        nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-        nmap("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-        -- nmap("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-        -- nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-        nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
         nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+
+        -- Jump to the definition of the word under your cursor.
+        --  This is where a variable was first declared, or where a function is defined, etc.
+        --  To jump back, press <C-T>.
+        nmap("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+
+        -- Find references for the word under your cursor.
+        nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+
         nmap("<leader>s", vim.lsp.buf.signature_help, "Signature Documentation")
 
         if data.name == "tailwind" then
           vim.keymap.set({ "n", "i", "x", "s" }, "<C-s>", "<Esc>:wa<cr>:TailwindSort<cr>")
         end
 
-        -- Lesser used LSP functionality
-        nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-        nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-        nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-        nmap("<leader>wl", function()
-          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, "[W]orkspace [L]ist Folders")
-
         -- Create a command `:Format` local to the LSP buffer
         vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
           vim.lsp.buf.format()
         end, { desc = "Format current buffer with LSP" })
+
+        -- The following two autocommands are used to highlight references of the
+        -- word under your cursor when your cursor rests there for a little while.
+        --    See `:help CursorHold` for information about when this is executed
+        --
+        -- When you move your cursor, the highlights will be cleared (the second autocommand).
+        local client = vim.lsp.get_client_by_id(data.client_id)
+        if client and client.server_capabilities.documentHighlightProvider then
+          vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+            buffer = bufnr,
+            callback = vim.lsp.buf.document_highlight,
+          })
+
+          vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+            buffer = bufnr,
+            callback = vim.lsp.buf.clear_references,
+          })
+        end
       end
 
-      local mason_lspconfig = require("mason-lspconfig")
-      mason_lspconfig.setup({
-        -- list of servers for mason to install
-        ensure_installed = vim.tbl_keys(servers),
-        -- auto-install configured servers (with lspconfig)
-        automatic_installation = true, -- not the same as ensure_installed
+      -- used to enable autocompletion (assign to every lsp server config)
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+      local ensure_installed = vim.tbl_keys(servers or {})
+      vim.list_extend(ensure_installed, {
+        "stylua", -- Used to format lua code
       })
-      mason_lspconfig.setup_handlers({
-        function(server_name)
-          require("lspconfig")[server_name].setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = servers[server_name],
-            filetypes = (servers[server_name] or {}).filetypes,
-          })
-        end,
+      require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
-        ["tailwindcss"] = function()
-          require("lspconfig").tailwindcss.setup({
-            hovers = true,
-            suggestions = true,
-            root_dir = function(fname)
-              vim.inspect(fname)
-              local root_pattern =
-                require("lspconfig").util.root_pattern("tailwind.config.cjs", "tailwind.config.js", "tailwind.config.ts", "postcss.config.js")
-              vim.inspect(root_pattern(fname), fname)
-              return root_pattern(fname)
-            end,
-          })
-        end,
+      require("mason-lspconfig").setup({
+        automatic_installation = true, -- not the same as ensure_installed
+
+        handlers = {
+          function(server_name)
+            local server = servers[server_name] or {}
+            require("lspconfig")[server_name].setup({
+              cmd = server.cmd,
+              settings = server.settings,
+              filetypes = server.filetypes,
+              capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {}),
+              on_attach = on_attach,
+            })
+          end,
+
+          ["tailwindcss"] = function()
+            require("lspconfig").tailwindcss.setup({
+              hovers = true,
+              suggestions = true,
+              root_dir = function(fname)
+                vim.inspect(fname)
+                local root_pattern =
+                  require("lspconfig").util.root_pattern("tailwind.config.cjs", "tailwind.config.js", "tailwind.config.ts", "postcss.config.js")
+                vim.inspect(root_pattern(fname), fname)
+                return root_pattern(fname)
+              end,
+            })
+          end,
+        },
       })
     end,
   },
