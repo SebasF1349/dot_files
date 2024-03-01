0d1fe41df9e0630bd9c74fedc570c0065a9e194f
SebasF1349 <ratablanca711@gmail.com>
2024-03-01 14:55:00 -0300

    feat: better markdown options

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -183,7 +183,16 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = "markdown",
-  command = "set awa",
+  callback = function()
+    vim.opt_local.textwidth = 80
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = "es"
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.expandtab = true
+  end,
 })
 
 -- Easier macros
