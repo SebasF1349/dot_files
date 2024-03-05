8f6f8519d54547e9a5fc8a2dbec99471cef5cf0d
SebasF1349 <ratablanca711@gmail.com>
2024-03-05 13:14:30 -0300

    chore: revemo resize font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -33,7 +33,7 @@ elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
 	--config.default_cwd = './~'
 end
 
-config.adjust_window_size_when_changing_font_size = false
+--config.adjust_window_size_when_changing_font_size = false
 config.hide_tab_bar_if_only_one_tab = true
 config.inactive_pane_hsb = {
 	saturation = 1,
@@ -77,47 +77,6 @@ wezterm.on("user-var-changed", function(window, _, name, value)
 	recompute_padding(window, value)
 end)
 
-local function recompute_font_size(window)
-	window:toast_notification("test", 12)
-	local window_dims = window:get_dimensions()
-	local overrides = window:get_config_overrides() or {}
-	local font_pixel_size = overrides.font_size * window_dims.dpi / 72
-	local number_rows = math.floor(window_dims / font_pixel_size)
-	local rest = window_dims % font_pixel_size
-	local new_pixel_font_size = font_pixel_size + rest / number_rows
-	local new_font_size = new_pixel_font_size * 72 / window_dims.dpi
-	overrides.font_size = new_font_size
-	window:set_config_overrides(overrides)
-	-- if not window_dims.is_full_screen then
-	-- 	if not overrides.window_padding then
-	-- 		-- not changing anything
-	-- 		return
-	-- 	end
-	-- 	overrides.window_padding = nil
-	-- else
-	-- 	-- Use only the middle 33%
-	-- 	local third = math.floor(window_dims.pixel_width / 3)
-	-- 	local new_padding = {
-	-- 		left = third,
-	-- 		right = third,
-	-- 		top = 0,
-	-- 		bottom = 0,
-	-- 	}
-	-- 	if overrides.window_padding and new_padding.left == overrides.window_padding.left then
-	-- 		-- padding is same, avoid triggering further changes
-	-- 		return
-	-- 	end
-	-- 	overrides.window_padding = new_padding
-	-- end
-end
-
-wezterm.on("window-resized", function(window)
-	recompute_font_size(window)
-end)
-
-wezterm.on("window-config-reloaded", function(window)
-	recompute_font_size(window)
-end)
 -- config.underline_thickness = 3
 -- config.cursor_thickness = 4
 -- config.underline_position = -6
