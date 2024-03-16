ea7a6ea9134d3d740695767b920c50a3c10e0727
SebasF1349 <ratablanca711@gmail.com>
2024-03-15 23:17:25 -0300

    feat(nv): remove todo-comments and add telescope ft

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -5,24 +5,6 @@ return {
   -- Detect tabstop and shiftwidth automatically
   { "NMAC427/guess-indent.nvim", opts = {} },
 
-  -- Highlight todo, notes, etc in comments
-  {
-    "folke/todo-comments.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local todo = require("todo-comments")
-      todo.setup({ signs = false })
-
-      vim.keymap.set("n", "]t", function()
-        todo.jump_next()
-      end, { desc = "Next todo comment" })
-      vim.keymap.set("n", "[t", function()
-        todo.jump_prev()
-      end, { desc = "Previous todo comment" })
-    end,
-  },
-
   {
     "m4xshen/hardtime.nvim",
     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
