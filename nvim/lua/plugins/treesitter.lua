8a8eb6f5e756315e0f1ecea348dff30677176df6
SebasF1349 <ratablanca711@gmail.com>
2024-03-17 14:12:23 -0300

    chore(nv): update plugins + fix ts build

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   event = { "BufReadPost", "BufNewFile" },
-  build = "<cmd>TSUpdate",
+  build = ":TSUpdate",
   config = function()
     vim.defer_fn(function()
       require("nvim-treesitter.configs").setup({
