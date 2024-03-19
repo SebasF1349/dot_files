5342d606b22c712ce0968f6ab9ef389cff2a2485
SebasF1349 <ratablanca711@gmail.com>
2024-03-18 21:27:51 -0300

    chore(nv): add colorcolumn to md files

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -168,6 +168,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = "markdown",
   callback = function()
     vim.opt_local.textwidth = 80
+    vim.opt_local.colorcolumn = "81"
     vim.opt_local.wrap = true
     vim.opt_local.spell = true
     vim.opt_local.spelllang = "es"
