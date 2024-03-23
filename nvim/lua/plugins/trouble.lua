d347b513c3eb391ad7ffde45821ac4c0ebdec762
SebasF1349 <ratablanca711@gmail.com>
2024-03-23 15:08:12 -0300

    chore(nv): more robust trouble keymaps

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -3,20 +3,25 @@ return {
   keys = { "<leader>x" },
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
+    local trouble = require("trouble")
     vim.keymap.set("n", "<leader>xx", function()
-      require("trouble").toggle()
+      trouble.toggle()
     end, { desc = "[X]Trouble [X]Toogle" })
     vim.keymap.set("n", "<leader>xw", function()
-      require("trouble").toggle("workspace_diagnostics")
+      trouble.toggle("workspace_diagnostics")
     end, { desc = "[X]Trouble [W]orkspace" })
     vim.keymap.set("n", "<leader>xd", function()
-      require("trouble").toggle("document_diagnostics")
+      trouble.toggle("document_diagnostics")
     end, { desc = "[X]Trouble [D]ocument" })
-    vim.keymap.set("n", "[x", function()
-      require("trouble").next({ skip_groups = true, jump = true })
-    end, { desc = "Next [T]rouble group" })
     vim.keymap.set("n", "]x", function()
-      require("trouble").previous({ skip_groups = true, jump = true })
+      if trouble.is_open() then
+        trouble.next({ skip_groups = true, jump = true })
+      end
+    end, { desc = "Next [T]rouble group" })
+    vim.keymap.set("n", "[x", function()
+      if trouble.is_open() then
+        trouble.previous({ skip_groups = true, jump = true })
+      end
     end, { desc = "Previous [T]rouble group" })
   end,
 }
