80091536fc24731b8ec936a0b8fad7878c6ef3cc
SebasF1349 <ratablanca711@gmail.com>
2024-03-27 12:00:59 -0300

    feat(nv): add new treesitter parser + filetypes

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,6 +4,14 @@ return {
   build = ":TSUpdate",
   dependencies = { "andymass/vim-matchup", opts = {} },
   config = function()
+    vim.filetype.add({
+      extension = { rasi = "rasi" },
+      pattern = {
+        [".*/hypr/.*%.conf"] = "hyprlang",
+        [".*/waybar/config"] = "jsonc",
+      },
+    })
+
     require("nvim-treesitter.configs").setup({
       ensure_installed = {
         "c",
@@ -15,6 +23,7 @@ return {
         "tsx",
         "javascript",
         "typescript",
+        "java",
         "vimdoc",
         "vim",
         "bash",
@@ -22,10 +31,18 @@ return {
         "html",
         "svelte",
         "json",
+        "jsonc",
         "toml",
         "markdown",
         "markdown_inline",
         "regex",
+        "hyprlang",
+        "git_config",
+        "dockerfile",
+        "sql",
+        "rasi",
+        "readline",
+        "yaml",
       },
 
       auto_install = true,
