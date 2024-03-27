595c7760a01381358ffedea13d25eb83e6222229
SebasF1349 <ratablanca711@gmail.com>
2024-03-27 11:42:29 -0300

    chore(nv): remove apparently unnecessary treesitter setup call

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,54 +4,47 @@ return {
   build = ":TSUpdate",
   dependencies = { "andymass/vim-matchup", opts = {} },
   config = function()
-    vim.defer_fn(function()
-      require("nvim-treesitter.configs").setup({
-        -- Add languages to be installed here that you want installed for treesitter
-        ensure_installed = {
-          "c",
-          "cpp",
-          "go",
-          "lua",
-          "python",
-          "rust",
-          "tsx",
-          "javascript",
-          "typescript",
-          "vimdoc",
-          "vim",
-          "bash",
-          "css",
-          "html",
-          "svelte",
-          "json",
-          "toml",
-          "markdown",
-          "markdown_inline",
-          "regex",
-        },
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "c",
+        "cpp",
+        "go",
+        "lua",
+        "python",
+        "rust",
+        "tsx",
+        "javascript",
+        "typescript",
+        "vimdoc",
+        "vim",
+        "bash",
+        "css",
+        "html",
+        "svelte",
+        "json",
+        "toml",
+        "markdown",
+        "markdown_inline",
+        "regex",
+      },
 
-        -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-        auto_install = true,
-        -- Install languages synchronously (only applied to `ensure_installed`)
-        sync_install = false,
-        -- List of parsers to ignore installing
-        ignore_install = {},
-        -- You can specify additional Treesitter modules here: -- For example: -- playground = {--enable = true,-- },
-        modules = {},
+      auto_install = true,
+      sync_install = false,
+      ignore_install = {},
+      modules = {},
 
-        highlight = { enable = true },
-        -- indent = { enable = true }, -- doesn't work properly
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "gn",
-            node_incremental = "gn",
-            scope_incremental = "gs",
-            node_decremental = "gr",
-          },
+      highlight = { enable = true },
+      -- indent = { enable = true }, -- doesn't work properly
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "gn",
+          node_incremental = "gn",
+          scope_incremental = "gs",
+          node_decremental = "gr",
         },
-        matchup = { enable = true },
-      })
-    end, 0)
+      },
+      matchup = { enable = true },
+    })
   end,
 }
