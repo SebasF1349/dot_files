2597da58c944505099891f47dacc20e5a47015fe
SebasF1349 <ratablanca711@gmail.com>
2024-04-02 12:24:39 -0300

    chore(nv): subtle theme changes

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -18,7 +18,7 @@ return {
         dap_ui = true,
         mini = {
           enabled = true,
-          indentscope_color = "", -- catppuccin color (eg. `lavender`) Default: text
+          indentscope_color = "",
         },
         native_lsp = {
           enabled = true,
@@ -40,12 +40,53 @@ return {
         },
       },
       custom_highlights = function(colors)
-        -- make the references to the word under the cursor darker
-        -- probably making other things light too, but I want them to be different to when I select a word
-        -- og color is surface1 in both
         return {
+          FloatBorder = { fg = colors.blue, bg = colors.none },
+          FloatTitle = { fg = colors.lavender, bg = colors.none },
+
+          LineNr = { fg = colors.overlay0 },
+
+          -- make the references to the word under the cursor darker
           LspReferenceRead = { bg = colors.surface0 },
           LspReferenceWrite = { bg = colors.surface0 },
+
+          MasonNormal = { link = "NormalFloat" },
+
+          PanelHeading = {
+            fg = colors.lavender,
+            bg = colors.none,
+            style = { "bold", "italic" },
+          },
+
+          LazyH1 = {
+            bg = colors.none,
+            fg = colors.lavender,
+            style = { "bold" },
+          },
+          LazyButton = {
+            bg = colors.none,
+            fg = colors.overlay0,
+          },
+          LazyButtonActive = {
+            bg = colors.none,
+            fg = colors.lavender,
+            style = { "bold" },
+          },
+          LazySpecial = { fg = colors.green },
+
+          PmenuSel = { bg = colors.green, fg = colors.base },
+
+          CmpItemAbbrMatch = { fg = colors.blue, style = { "bold" } },
+          CmpItemMenu = { fg = colors.subtext1 },
+          CmpDoc = { link = "NormalFloat" },
+          CmpDocBorder = { fg = colors.surface1, bg = colors.none },
+
+          TroubleNormal = { bg = colors.none },
+
+          TelescopeMatching = { fg = colors.lavender },
+          TelescopeResultsDiffAdd = { fg = colors.green },
+          TelescopeResultsDiffChange = { fg = colors.yellow },
+          TelescopeResultsDiffDelete = { fg = colors.red },
         }
       end,
     })
