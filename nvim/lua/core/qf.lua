3b995cabe505d8a516c9b8f0c76be96fc2fac0bb
SebasF1349 <ratablanca711@gmail.com>
2024-05-03 22:27:14 -0300

    chore(nv): reorganize qf keymaps

--- nvim/lua/core/qf.lua
+++ nvim/lua/core/qf.lua
@@ -159,31 +159,8 @@ local function cprev_wrap()
     vim.cmd("clast")
   end
 end
-
-local function getPath(line)
-  local path, _ = line:gsub("│.*$", "")
-  return path
-end
-
-local function jumpFileChunk(move)
-  local path = getPath(vim.fn.getline("."))
-  local direction = move == "down" and "j" or "k"
-  local finish = move == "down" and "$" or 1
-
-  while path == getPath(vim.fn.getline(".")) and vim.fn.getline(".") ~= vim.fn.getline(finish) do
-    vim.cmd("normal! " .. direction)
-  end
-  vim.cmd("normal o")
-end
-
 vim.keymap.set("n", "]q", cnext_wrap, { desc = "Next [Q]uickfix Item Wrapping" })
 vim.keymap.set("n", "[q", cprev_wrap, { desc = "Previous [Q]uickfix Item Wrapping" })
-vim.keymap.set("n", "]Q", function()
-  jumpFileChunk("down")
-end, { desc = "Next [Q]uickfix File" })
-vim.keymap.set("n", "[Q", function()
-  jumpFileChunk("up")
-end, { desc = "Previous [Q]uickfix File" })
 
 --------------------------------------------------
 -- Quickfix Autocmds
@@ -273,49 +250,75 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
   desc = "Qf syntax + options",
 })
 
+--------------------------------------------------
+-- Keymaps inside Quickfix
+--------------------------------------------------
+
+local function getPath(line)
+  local path, _ = line:gsub("│.*$", "")
+  return path
+end
+
+local function jumpFileChunk(move)
+  local path = getPath(vim.fn.getline("."))
+  local direction = move == "down" and "j" or "k"
+  local finish = move == "down" and "$" or 1
+
+  while path == getPath(vim.fn.getline(".")) and vim.fn.getline(".") ~= vim.fn.getline(finish) do
+    vim.cmd("normal! " .. direction)
+  end
+  vim.cmd("normal o")
+end
+
+-- https://gitlab.com/ranjithshegde/dotbare/-/blob/master/.config/nvim/lua/r/extensions/qf.lua
+local function t_filter(item)
+  return item ~= false
+end
+
+local function delete(bufnr)
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
+  local qfl = vim.fn.getqflist()
+
+  local mode = vim.fn.mode()
+  if mode == "v" or mode == "V" then
+    local _, ls, _ = unpack(vim.fn.getpos("v"))
+    local _, le, _ = unpack(vim.fn.getpos("."))
+    for i = ls, le do
+      qfl[i] = false
+    end
+    qfl = vim.tbl_filter(t_filter, qfl)
+    vim.fn.setqflist({}, "r", {
+      title = "All Diagnostics",
+      items = qfl,
+    })
+    vim.api.nvim_input("<Esc>")
+  else
+    local line = unpack(vim.api.nvim_win_get_cursor(0))
+    table.remove(qfl, line)
+    vim.fn.setqflist({}, "r", {
+      title = "All Diagnostics",
+      items = qfl,
+    })
+    vim.fn.setpos(".", { bufnr, line, 1, 0 })
+  end
+end
+
 vim.api.nvim_create_autocmd("BufWinEnter", {
   group = qf_group,
   pattern = "quickfix",
   callback = function()
-    -- https://gitlab.com/ranjithshegde/dotbare/-/blob/master/.config/nvim/lua/r/extensions/qf.lua
-    local function t_filter(item)
-      return item ~= false
-    end
-
-    local function delete(bufnr)
-      bufnr = bufnr or vim.api.nvim_get_current_buf()
-      local qfl = vim.fn.getqflist()
-
-      local mode = vim.fn.mode()
-      if mode == "v" or mode == "V" then
-        local _, ls, _ = unpack(vim.fn.getpos("v"))
-        local _, le, _ = unpack(vim.fn.getpos("."))
-        for i = ls, le do
-          qfl[i] = false
-        end
-        qfl = vim.tbl_filter(t_filter, qfl)
-        vim.fn.setqflist({}, "r", {
-          title = "All Diagnostics",
-          items = qfl,
-        })
-        vim.api.nvim_input("<Esc>")
-      else
-        local line = unpack(vim.api.nvim_win_get_cursor(0))
-        table.remove(qfl, line)
-        vim.fn.setqflist({}, "r", {
-          title = "All Diagnostics",
-          items = qfl,
-        })
-        vim.fn.setpos(".", { bufnr, line, 1, 0 })
-      end
-    end
-
     -- vim.keymap.set("n", "j", "<down><CR><C-w>p", { buffer = 0, desc = "Next QF Item" })
     -- vim.keymap.set("n", "k", "<up><CR><C-w>p", { buffer = 0, desc = "Previous QF Item" })
     vim.keymap.set("n", "o", "<CR><C-w>p", { buffer = 0, desc = "Open and Stay in QF" })
     vim.keymap.set("n", "O", "<CR><cmd>cclose<CR>", { buffer = 0, desc = "Open and Close QF" })
     vim.keymap.set("n", "dd", delete, { buffer = 0, desc = "Delete QF Item" })
     vim.keymap.set({ "v" }, "d", delete, { buffer = 0, desc = "Delete QF Item" })
+    vim.keymap.set("n", "]Q", function()
+      jumpFileChunk("down")
+    end, { desc = "Next [Q]uickfix File" })
+    vim.keymap.set("n", "[Q", function()
+      jumpFileChunk("up")
+    end, { desc = "Previous [Q]uickfix File" })
   end,
   once = true,
   desc = "Keymaps inside quickfix window",
