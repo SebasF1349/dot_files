e4488a08775c68f34a53827093fe1ad4c7035036
SebasF1349 <ratablanca711@gmail.com>
2024-05-07 21:28:44 -0300

    chore(nv): make Messages wrap

--- nvim/lua/core/cmds.lua
+++ nvim/lua/core/cmds.lua
@@ -4,4 +4,5 @@ vim.api.nvim_create_user_command("Messages", function()
   local messages = vim.split(vim.fn.execute("messages", "silent"), "\n")
   vim.api.nvim_buf_set_text(scratch_buffer, 0, 0, 0, 0, messages)
   vim.cmd("vertical sbuffer " .. scratch_buffer)
+  vim.opt_local.wrap = true
 end, {})
