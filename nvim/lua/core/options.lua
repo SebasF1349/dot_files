850864f5d03bf5a017914371006e7ee1c800b7d7
SebasF1349 <ratablanca711@gmail.com>
2024-05-13 17:13:25 -0300

    fix(nv): fully disable mouse scrolling

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -13,6 +13,10 @@ vim.opt.showcmd = false
 
 -- Disable mouse mode
 vim.o.mouse = ""
+vim.keymap.set("", "<up>", "<nop>", { noremap = true })
+vim.keymap.set("", "<down>", "<nop>", { noremap = true })
+vim.keymap.set("i", "<up>", "<nop>", { noremap = true })
+vim.keymap.set("i", "<down>", "<nop>", { noremap = true })
 
 -- Enable break indent
 vim.o.breakindent = true
