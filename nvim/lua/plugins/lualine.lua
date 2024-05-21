5e61aeb16ac0ec1c8842012f3031d02aaed87956
SebasF1349 <ratablanca711@gmail.com>
2024-05-21 18:45:36 -0300

    feat(nv): better and cleaner diagnostics in lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,9 +1,69 @@
 local mocha = require("catppuccin.palettes").get_palette("mocha")
 
+local signs = {
+  " ",
+  " ",
+  "",
+  " ",
+}
+local levels = {
+  vim.diagnostic.severity.ERROR,
+  vim.diagnostic.severity.WARN,
+  vim.diagnostic.severity.INFO,
+  vim.diagnostic.severity.HINT,
+}
+local levels_hi = {
+  "StatusLineError",
+  "StatusLineWarn",
+  "StatusLineInfo",
+  "StatusLineHint",
+}
+
+vim.api.nvim_set_hl(0, levels_hi[1], { fg = mocha.red, bg = mocha.surface0 })
+vim.api.nvim_set_hl(0, levels_hi[2], { fg = mocha.yellow, bg = mocha.surface0 })
+vim.api.nvim_set_hl(0, levels_hi[3], { fg = mocha.sky, bg = mocha.surface0 })
+vim.api.nvim_set_hl(0, levels_hi[4], { fg = mocha.teal, bg = mocha.surface0 })
+
+local function local_diagnostics()
+  for i, _ in ipairs(levels) do
+    local count = vim.tbl_count(vim.diagnostic.get(0, { severity = i }))
+    if count > 0 then
+      return "%#" .. levels_hi[i] .. "#" .. signs[i]
+    end
+  end
+
+  return ""
+end
+
+vim.api.nvim_set_hl(0, "StatusLineWorkspace", { fg = mocha.comment, bg = mocha.surface0 })
+
+local function workspace_diagnostics()
+  for i, _ in ipairs(levels) do
+    local count = vim.tbl_count(vim.diagnostic.get(nil, { severity = i }))
+    local local_count = vim.tbl_count(vim.diagnostic.get(0, { severity = i }))
+    if count > local_count then
+      return "%#StatusLineWorkspace#" .. signs[i]
+    end
+  end
+
+  return ""
+end
+
+local function custom_diagnostics()
+  local local_diag = local_diagnostics()
+  local workspace_diag = workspace_diagnostics()
+  if #local_diag == 0 then
+    return workspace_diag
+  elseif #workspace_diag == 0 then
+    return local_diag
+  else
+    return local_diag .. " " .. workspace_diag
+  end
+end
+
 return {
   "nvim-lualine/lualine.nvim",
   event = { "BufReadPre", "BufNewFile" },
-  -- event = "VeryLazy",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
     options = {
@@ -39,15 +99,10 @@ return {
           end,
           color = { fg = mocha.yellow },
         },
-        "diagnostics",
       },
       lualine_c = {},
       lualine_x = {},
-      lualine_y = { {
-        "diagnostics",
-        sources = { "nvim_workspace_diagnostic" },
-        sections = { "error", "warn" },
-      } },
+      lualine_y = { custom_diagnostics },
       lualine_z = { "grapple" },
     },
     inactive_sections = {},
@@ -61,7 +116,7 @@ return {
       lualine_z = { { "filename", path = 1, symbols = { unnamed = "" } } },
     },
     inactive_winbar = {
-      lualine_a = { "diagnostics" },
+      lualine_a = {},
       lualine_b = {},
       lualine_c = {},
       lualine_x = {},
