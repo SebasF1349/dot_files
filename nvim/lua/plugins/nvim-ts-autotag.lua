4c0190fac22cb238ebb1fec9ee033a51166b8b42
SebasF1349 <ratablanca711@gmail.com>
2024-05-21 16:11:58 -0300

    chore(nv): ts-autotag updated config

--- nvim/lua/plugins/nvim-ts-autotag.lua
+++ nvim/lua/plugins/nvim-ts-autotag.lua
@@ -1,14 +1,12 @@
 return {
   "windwp/nvim-ts-autotag",
   ft = { "html", "svelte", "markdown" },
-  dependencies = "nvim-treesitter/nvim-treesitter",
   config = function()
     require("nvim-ts-autotag").setup({
-      enable = true,
-      enable_rename = true,
-      enable_close = true,
-      enable_close_on_clash = true,
-      filetypes = { "svelte", "html", "markdown" },
+      opts = {
+        enable_close_on_clash = true,
+        filetypes = { "svelte", "html", "markdown" },
+      },
     })
   end,
 }
