559bdb13e2c126616e4709c2a89f998cb1efba4b
SebasF1349 <ratablanca711@gmail.com>
2024-06-13 12:11:38 -0300

    fix(nv): add filetypes to jdtls

--- nvim/lua/plugins/jdtls.lua
+++ nvim/lua/plugins/jdtls.lua
@@ -60,6 +60,7 @@ return {
           inlayHints = {
             parameterNames = {
               enabled = "all",
+        filetypes = { "java" },
             },
           },
           sources = {
