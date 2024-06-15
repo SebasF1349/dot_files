df2f5d74e362b47f18e3bc2f3b200b49b29b1376
SebasF1349 <ratablanca711@gmail.com>
2024-06-15 13:53:24 -0300

    refactor(nv): qf delete function

--- nvim/lua/core/qf.lua
+++ nvim/lua/core/qf.lua
@@ -482,11 +482,6 @@ local function getMessage(line)
   return path
 end
 
----@param item table | boolean
-local function t_filter(item)
-  return item ~= false
-end
-
 ---@param bufnr number
 local function delete(bufnr)
   bufnr = bufnr or vim.api.nvim_get_current_buf()
@@ -494,23 +489,25 @@ local function delete(bufnr)
 
   local mode = vim.fn.mode()
   if mode == "v" or mode == "V" then
-    local _, ls, _ = unpack(vim.fn.getpos("v"))
-    local _, le, _ = unpack(vim.fn.getpos("."))
-    for i = ls, le do
-      qfl[i] = false
+    local visual_start = vim.fn.getpos("v")
+    local visual_end = vim.fn.getpos(".")
+    for i = visual_start[2], visual_end[2] do
+      qfl[i] = {}
     end
-    qfl = vim.tbl_filter(t_filter, qfl)
+    qfl = vim.tbl_filter(function(item)
+      return not vim.tbl_isempty(item)
+    end, qfl)
     vim.fn.setqflist({}, "r", {
       items = qfl,
     })
     vim.api.nvim_input("<Esc>")
   else
-    local line = unpack(vim.api.nvim_win_get_cursor(0))
-    table.remove(qfl, line)
+    local line = vim.api.nvim_win_get_cursor(0)
+    table.remove(qfl, line[1])
     vim.fn.setqflist({}, "r", {
       items = qfl,
     })
-    vim.fn.setpos(".", { bufnr, line, 1, 0 })
+    vim.api.nvim_win_set_cursor(0, { line[1], 0 })
   end
 end
 
