cf945745de49ffd6929e38bd4c254167875d7028
SebasF1349 <ratablanca711@gmail.com>
2024-06-17 15:42:09 -0300

    fix(nv): fix version utils because 0 is true in lua

--- nvim/lua/utils/nvim-version.lua
+++ nvim/lua/utils/nvim-version.lua
@@ -4,15 +4,15 @@ local current_version = "nvim-0.10"
 local nightly_version = "nvim-0.11"
 
 M.is_old = function()
-  return not vim.fn.has(current_version)
+  return vim.fn.has(current_version) == 0
 end
 
 M.is_stable = function()
-  return vim.fn.has(current_version) and not vim.fn.has(nightly_version)
+  return vim.fn.has(current_version) == 1 and vim.fn.has(nightly_version) == 0
 end
 
 M.is_nightly = function()
-  return vim.fn.has(nightly_version)
+  return vim.fn.has(nightly_version) == 1
 end
 
 return M
