fa012b1f19f73b86d4e1bdeee96ddeb8e77a50bf
SebasF1349 <ratablanca711@gmail.com>
2024-06-26 14:25:15 -0300

    chore(nv): only use flash for f/t/F/T

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -6,10 +6,6 @@ return {
     { "F", mode = { "n", "x", "o" } },
     { "t", mode = { "n", "x", "o" } },
     { "T", mode = { "n", "x", "o" } },
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump({ remote_op = { restore = true, motion = true }, }) end, desc = "Fla[S]h Jump",},
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter [S]election",},
-    { "gs", mode = { "n", "x", "o" }, function() require("flash").treesitter({ jump = { pos = "start" }, label = { before = true, after = false }, }) end, desc = "Flash Treesitter [G]o [S]tart",},
-    { "ge", mode = { "n", "x", "o" }, function() require("flash").treesitter({ jump = { pos = "end" }, label = { before = false, after = true }, }) end, desc = "Flash Treesitter [G]o [E]nd",},
   },
   opts = {
     modes = {
