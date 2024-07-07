1c2bc83731e4765fc6cfb75aa1e669a0d1b606ea
SebasF1349 <ratablanca711@gmail.com>
2024-07-07 20:47:39 -0300

    feat(nv): better default movements
    remove nvim-spider for simpler and better movements for b/e/ge/w/{/}
    without changing operator pending movements (y/c/d)

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -12,32 +12,4 @@ return {
       crates.show()
     end,
   },
-
-  {
-    -- improve wordwise-movements to skip insignificant punctuation
-    -- note that it changes how [d|c|y]w work, use [d|c|y]iw
-    'chrisgrieser/nvim-spider',
-    keys = {
-      {
-        'w',
-        "<cmd>lua require('spider').motion('w')<CR>", -- using lua breaks dot-repeaetebility
-        mode = { 'n', 'o', 'x' },
-      },
-      {
-        'e',
-        "<cmd>lua require('spider').motion('e')<CR>",
-        mode = { 'n', 'o', 'x' },
-      },
-      {
-        'b',
-        "<cmd>lua require('spider').motion('b')<CR>",
-        mode = { 'n', 'o', 'x' },
-      },
-    },
-    opts = {
-      skipInsignificantPunctuation = true,
-      consistentOperatorPending = false,
-      subwordMovement = false, -- don't needed with mini.ai subword textobject
-    },
-  },
 }
