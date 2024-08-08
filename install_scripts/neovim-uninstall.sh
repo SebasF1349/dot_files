8a9633dd9df844c0394a4de5d4003665ef123498
SebasF1349 <ratablanca711@gmail.com>
2024-08-08 11:29:30 -0300

    fix(install): remove neovim directory with sudo

--- install_scripts/neovim-uninstall.sh
+++ install_scripts/neovim-uninstall.sh
@@ -9,4 +9,4 @@ sudo rm /usr/local/share/man/man1/nvim.1
 sudo rm /usr/local/share/applications/nvim.desktop
 sudo rm /usr/local/bin/nvim
 sudo rm -rf /usr/local/share/nvim/
-rm -rf "$HOME/apps/neovim"
+sudo rm -rf "$HOME/apps/neovim"
