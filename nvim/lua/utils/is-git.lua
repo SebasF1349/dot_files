ea6d65f0d93d0a4fe09979049869df27126e2bfc
SebasF1349 <ratablanca711@gmail.com>
2024-08-19 19:44:29 -0300

    fix(nv): make is_git os independent

--- nvim/lua/utils/is-git.lua
+++ nvim/lua/utils/is-git.lua
@@ -3,7 +3,7 @@ local is_git
 ---@return boolean
 local M = function()
   if is_git == nil then
-    is_git = vim.fs.root(vim.env.PWD, '.git') ~= nil
+    is_git = vim.fs.root(vim.uv.cwd() or 0, '.git') ~= nil
   end
   return is_git
 end
