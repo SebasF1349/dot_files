7c9da19c165820716b727767091a14780f467468
SebasF1349 <ratablanca711@gmail.com>
2024-08-28 13:47:38 -0300

    feat(wez): recognize vim as nvim

--- wezterm/utils.lua
+++ wezterm/utils.lua
@@ -1,8 +1,11 @@
 local M = {}
 
 function M.is_nvim(pane)
-	return pane:get_foreground_process_name():find("n?vim")
-		or (pane:get_user_vars().WEZTERM_PROG and pane:get_user_vars().WEZTERM_PROG:find("nv"))
+	return pane:get_foreground_process_name():find("vim")
+		or (
+			pane:get_user_vars().WEZTERM_PROG
+			and (pane:get_user_vars().WEZTERM_PROG:find("nv") or pane:get_user_vars().WEZTERM_PROG:find("vim"))
+		)
 end
 
 return M
