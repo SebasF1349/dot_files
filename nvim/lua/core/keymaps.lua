eafad28cfa22fa4fd2283b3d496679294bec9284
SebasF1349 <ratablanca711@gmail.com>
2024-10-03 10:19:02 -0300

    fix(nv): substitute now works with yanked multiple lines

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -474,15 +474,19 @@ end
 ---@param mode "char"|"line"|"block"
 function _G.Substitute(mode)
   local reg = vim.fn.getreg('"')
+  local text = vim.split(reg, '\n')
+  if text[text] == '' then
+    table.remove(text)
+  end
   local starting = vim.api.nvim_buf_get_mark(0, '[')
   local ending = vim.api.nvim_buf_get_mark(0, ']')
   if mode == 'char' then
-    vim.api.nvim_buf_set_text(0, starting[1] - 1, starting[2], ending[1] - 1, ending[2] + 1, { reg })
+    vim.api.nvim_buf_set_text(0, starting[1] - 1, starting[2], ending[1] - 1, ending[2] + 1, text)
   elseif mode == 'line' then
-    vim.api.nvim_buf_set_lines(0, starting[1] - 1, ending[1], true, { reg })
+    vim.api.nvim_buf_set_lines(0, starting[1] - 1, ending[1], true, text)
   elseif mode == 'block' then
     for i = starting[1] - 1, ending[1] - 1 do
-      vim.api.nvim_buf_set_text(0, i, starting[2], i, ending[2] + 1, { reg })
+      vim.api.nvim_buf_set_text(0, i, starting[2], i, ending[2] + 1, text)
     end
   end
 end
