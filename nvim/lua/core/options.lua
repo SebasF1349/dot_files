c8e0b2f1318765189927c626bf171ab20235fc86
SebasF1349 <ratablanca711@gmail.com>
2024-10-10 20:32:47 -0300

    feat(nv): improve fold virtual text

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -97,8 +97,56 @@ vim.opt.smoothscroll = true
 -- folds
 vim.opt.foldmethod = 'expr'
 vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-vim.opt.foldtext = ''
 vim.opt.foldlevelstart = 99
+
+-- https://new.reddit.com/r/neovim/comments/1fzn1zt/custom_fold_text_function_with_treesitter_syntax/
+local function fold_virt_text(result, s, lnum, coloff)
+  if not coloff then
+    coloff = 0
+  end
+  local text = ''
+  local hl
+  for i = 1, #s do
+    local char = s:sub(i, i)
+    local hls = vim.treesitter.get_captures_at_pos(0, lnum, coloff + i - 1)
+    local _hl = hls[#hls]
+    if _hl then
+      local new_hl = '@' .. _hl.capture
+      if new_hl ~= hl then
+        table.insert(result, { text, hl })
+        text = ''
+        hl = nil
+      end
+      text = text .. char
+      hl = new_hl
+    else
+      text = text .. char
+    end
+  end
+  table.insert(result, { text, hl })
+end
+
+function _G.custom_foldtext()
+  local start = vim.fn.getline(vim.v.foldstart):gsub('\t', string.rep(' ', vim.o.tabstop))
+  local end_str = vim.fn.getline(vim.v.foldend)
+  local end_ = vim.trim(end_str)
+  local result = {}
+  fold_virt_text(result, start, vim.v.foldstart - 1)
+  local number_lines = vim.v.foldend - vim.v.foldstart
+  table.insert(result, { ' [ ' .. number_lines .. ' lines ] ', 'Delimiter' })
+  fold_virt_text(result, end_, vim.v.foldend - 1, #(end_str:match('^(%s+)') or ''))
+  return result
+end
+
+vim.opt.foldtext = 'v:lua.custom_foldtext()'
+-- vim.opt.foldtext = ''
+
+vim.opt.fillchars:append({
+  fold = ' ',
+  foldopen = ' ',
+  foldclose = ' ',
+  foldsep = ' ',
+})
 
 -- Nicer and less noicy signcolumn
 vim.opt.signcolumn = 'yes'
