55b6b911e3911795c4aa80be2e1f35e80c27cf08
SebasF1349 <ratablanca711@gmail.com>
2024-11-05 18:36:32 -0300

    change(nv): gitsigns signs

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,23 +1,22 @@
 return {
-  -- Adds git related signs to the gutter, as well as utilities for managing changes
   'lewis6991/gitsigns.nvim',
   event = { 'BufReadPre', 'BufNewFile' },
   opts = {
     signs = {
-      add = { text = '▎' },
-      change = { text = '▎' },
-      delete = { text = '' },
-      topdelete = { text = '' },
-      changedelete = { text = '▎' },
-      untracked = { text = '▎' },
+      add = { text = '│' },
+      change = { text = '│' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '└' },
+      untracked = { text = '┆' },
     },
     signs_staged = {
-      add = { text = '▎' },
-      change = { text = '▎' },
-      delete = { text = '' },
-      topdelete = { text = '' },
-      changedelete = { text = '▎' },
-      untracked = { text = '▎' },
+      add = { text = '│' },
+      change = { text = '│' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '└' },
+      untracked = { text = '┆' },
     },
     preview_config = { border = 'none' },
     on_attach = function()
