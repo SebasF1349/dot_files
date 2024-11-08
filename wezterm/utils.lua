fa8196868e70f64d4aed36956c9ed68919dc38b4
SebasF <33849340+SebasF1349@users.noreply.github.com>
2024-11-08 13:31:58 -0300

    fix(nv-wez): fix nvim-wezterm integration for windows

--- wezterm/utils.lua
+++ wezterm/utils.lua
@@ -1,10 +1,13 @@
 local M = {}
 
 function M.is_nvim(pane)
-	return pane:get_foreground_process_name():find("vim")
+	return pane:get_user_vars().IS_NVIM == "true"
+		or (pane:get_foreground_process_name() and (pane:get_foreground_process_name():find("nv") or pane
+			:get_foreground_process_name()
+			:find("n?vim")))
 		or (
 			pane:get_user_vars().WEZTERM_PROG
-			and (pane:get_user_vars().WEZTERM_PROG:find("nv") or pane:get_user_vars().WEZTERM_PROG:find("vim"))
+			and (pane:get_user_vars().WEZTERM_PROG:find("nv") or pane:get_user_vars().WEZTERM_PROG:find("n?vim"))
 		)
 end
 
