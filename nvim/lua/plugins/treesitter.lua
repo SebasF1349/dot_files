762bd74ba2548339905c6aad63eda5d23476aab3
SebasF1349 <ratablanca711@gmail.com>
2024-12-01 14:59:58 -0300

    fix(nv): broken fold text-objects

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -197,9 +197,9 @@ return {
       -- can use ['az'] = { query = '@fold', query_group = 'folds' }, but needs an offset for iz
       -- more robust option (do I want the if/else behaviour?) : https://vimways.org/2018/transactions-pending/
       vim.keymap.set('v', 'iz', ':<C-U>silent! normal! [zjV]zk<CR>', { desc = 'Fold Text-Object' })
-      vim.keymap.set('o', 'iz', '<cmd>normal Vii<CR>', { desc = 'Fold Text-Object', remap = false })
+      vim.keymap.set('o', 'iz', '<cmd>normal Viz<CR>', { desc = 'Fold Text-Object', remap = false })
       vim.keymap.set('v', 'az', ':<C-U>silent! normal! [zV]z<CR>', { desc = 'Fold Text-Object' })
-      vim.keymap.set('o', 'az', '<cmd>normal Vai<CR>', { desc = 'Fold Text-Object', remap = false })
+      vim.keymap.set('o', 'az', '<cmd>normal Vaz<CR>', { desc = 'Fold Text-Object', remap = false })
     end,
   },
   {
