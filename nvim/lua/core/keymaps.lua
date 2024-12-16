dddc5e2847b4dada01da6b6829293e5353fea968
SebasF1349 <ratablanca711@gmail.com>
2024-12-16 19:34:45 -0300

    chore(nv): add W to abbr

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -530,7 +530,7 @@ vim.keymap.set('n', '<C-a>', toggle_word, { desc = 'Toggle keywords' })
 -- Abbreviations
 --------------------------------------------------
 
-local cmds_typos = { 'Wa', 'WA' }
+local cmds_typos = { 'W', 'Wa', 'WA' }
 for _, cmd in ipairs(cmds_typos) do
   vim.keymap.set('ca', cmd, cmd:lower())
 end
