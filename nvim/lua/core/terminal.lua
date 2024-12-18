2e04fef76e7ac014f746ee5ff3bbd0048a9d14c1
SebasF1349 <ratablanca711@gmail.com>
2024-12-18 19:29:18 -0300

    reactor(nv): big terminal refactor without adding new features
    - using lua api instead of ex commands
    - reduced number of lines and number of loops
    - clearer code

--- nvim/lua/core/terminal.lua
+++ nvim/lua/core/terminal.lua
@@ -3,11 +3,13 @@
 --------------------------------------------------
 
 ---@class term
----@field buf_num number
----@field win_id number
+---@field bufnr number
+---@field winid number
 
 ---@type term[]
 local terms = {}
+
+local terminal_autocmds = vim.api.nvim_create_augroup('Terminal Autocmds', { clear = true })
 
 --------------------------------------------------
 -- Keymaps
@@ -16,8 +18,8 @@ local terms = {}
 vim.keymap.set('t', 'jk', '<C-\\><C-n>', { desc = 'Escape Terminal Mode' })
 
 vim.keymap.set({ 'n', 't' }, '<leader>tb', function()
-  local win = (terms[1].win_id and terms[1].win_id ~= -1) and terms[1].win_id or terms[2].win_id
-  if win and win ~= -1 and vim.api.nvim_win_is_valid(win) then
+  local win = vim.api.nvim_win_is_valid(terms[1].winid) and terms[1].winid or terms[2].winid
+  if vim.api.nvim_win_is_valid(win) then
     vim.api.nvim_set_current_win(win)
   end
 end, { desc = 'Move to [T]erminal [B]uffer ' })
@@ -26,53 +28,45 @@ end, { desc = 'Move to [T]erminal [B]uffer ' })
 -- Togglers
 --------------------------------------------------
 
----@param term term
-local function check_term_data(term)
-  if term.win_id ~= -1 and not vim.list_contains(vim.api.nvim_list_wins(), term.win_id) then
-    term.win_id = -1
-    if not vim.list_contains(vim.api.nvim_list_bufs(), term.buf_num) then
-      term.buf_num = -1
-    end
-  end
+---@param opts term
+local function set_term_opts(opts)
+  vim.cmd.startinsert()
+  vim.bo.filetype = 'terminal'
+  vim.bo.buflisted = false
+  vim.wo.statuscolumn = ''
+  vim.wo.winfixheight = true
+  vim.wo.winfixwidth = true
+  vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = opts.bufnr, silent = true })
+  vim.api.nvim_set_option_value('winhighlight', 'Normal:TerminalNormal', { win = opts.winid, scope = 'local' })
 end
 
-local term_cmd = 'botright vsplit | vertical resize 50 | set winfixwidth winfixheight'
+---@param bufnr number
+---@return term
+local function create_term(bufnr)
+  local buf = vim.api.nvim_buf_is_valid(bufnr) and bufnr or vim.api.nvim_create_buf(false, true)
+  local win = vim.api.nvim_open_win(buf, true, { split = 'right', win = 0, width = 50 })
+  local term = { bufnr = buf, winid = win }
+  if vim.bo[buf].buftype ~= 'terminal' then
+    vim.cmd.terminal()
+    set_term_opts(term)
+  end
+  return term
+end
 
 ---@param num 1|2
 local function toggle_term(num)
-  local term = terms[num]
-  check_term_data(term)
-  if term.buf_num == -1 then
-    vim.cmd(term_cmd .. '| term')
-    term.buf_num = vim.api.nvim_get_current_buf()
-    term.win_id = vim.api.nvim_get_current_win()
-    vim.cmd.startinsert()
-    vim.bo.filetype = 'terminal'
-    vim.bo.buflisted = false
-    vim.wo.statuscolumn = ''
-    vim.wo.winfixheight = true
-    vim.wo.winfixwidth = true
-    vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = term.buf_num, silent = true })
-    vim.api.nvim_set_option_value('winhighlight', 'Normal:TerminalNormal', { win = term.win_id, scope = 'local' })
-    vim.api.nvim_create_autocmd('BufEnter', {
-      buffer = term.buf_num,
-      callback = function()
-        vim.cmd.startinsert()
-      end,
-    })
-  elseif term.win_id == -1 then
-    vim.cmd(term_cmd .. '| b' .. term.buf_num)
-    term.win_id = vim.api.nvim_get_current_win()
+  local bufnr, winid = terms[num].bufnr, terms[num].winid
+  if not vim.api.nvim_win_is_valid(winid) then
+    terms[num] = create_term(bufnr)
   else
-    vim.api.nvim_win_close(term.win_id, true)
-    term.win_id = -1
+    vim.api.nvim_win_hide(winid)
   end
 end
 
 local TERMINALS_COUNT = 2
 
 for pos = 1, TERMINALS_COUNT do
-  terms[pos] = { buf_num = -1, win_id = -1, is_hidden = -1 }
+  terms[pos] = { bufnr = -1, winid = -1 }
   vim.keymap.set({ 'n', 't' }, '<leader>t' .. pos, function()
     toggle_term(pos)
   end, { desc = 'Toggle [T]erminal [' .. pos .. ']' })
@@ -85,6 +79,9 @@ end, { desc = '[T]oggle [T]erminal 1' })
 -- Runners
 --------------------------------------------------
 
+--- Move to end of file
+---@param bufnr number
+---@param winid number
 local function scroll_to_end(bufnr, winid)
   vim.api.nvim_buf_call(bufnr, function()
     local target_line = vim.tbl_count(vim.api.nvim_buf_get_lines(bufnr, 0, -1, true))
@@ -95,13 +92,11 @@ end
 ---@param cmd string
 local function run_term_command(cmd)
   vim.cmd('wa')
-  local term = terms[1]
-  check_term_data(term)
-  if term.win_id == -1 then
-    toggle_term(1)
+  if not vim.api.nvim_win_is_valid(terms[1].winid) then
+    terms[1] = create_term(terms[1].bufnr)
   end
-  scroll_to_end(term.buf_num, term.win_id)
-  local terminal_job_id = (vim.api.nvim_buf_get_var(term.buf_num, 'terminal_job_id'))
+  scroll_to_end(terms[1].bufnr, terms[1].winid)
+  local terminal_job_id = (vim.api.nvim_buf_get_var(terms[1].bufnr, 'terminal_job_id'))
   vim.api.nvim_chan_send(terminal_job_id, '\n')
   vim.api.nvim_chan_send(terminal_job_id, cmd .. '\n')
 end
@@ -132,8 +127,6 @@ end, { desc = '[C]ode [T]est' })
 --------------------------------------------------
 -- Autocmds
 --------------------------------------------------
-
-local terminal_autocmds = vim.api.nvim_create_augroup('Terminal Autocmds', { clear = true })
 
 vim.api.nvim_create_autocmd('WinEnter', {
   callback = function()
