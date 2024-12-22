eb0ee2a63058cf00c59711c79bcee172955c2d1f
SebasF1349 <ratablanca711@gmail.com>
2024-12-22 09:44:10 -0300

    feat(nv): bunch of lsp changes
    - trigger completion on InsertEnter
    - show documentation on completion
    - replace implementation of next/prev reference
        - copied from Folke's snacks
        - uses document_highlight so it's faster (as doesn't need new
          request)
        - needs immediate document_highlight
        - can use count now
    - use C-b and C-f to scroll docs float window
    - add space to diagnostics signs

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -87,46 +87,46 @@ return {
             return
           end
 
-          -- vim.api.nvim_create_autocmd({ 'InsertEnter' }, {
-          --   buffer = event.buf,
-          --   callback = function()
-          --     vim.lsp.completion.trigger()
-          --   end,
-          -- })
-          -- -- based on https://github.com/neovim/neovim/issues/29225#issuecomment-2159428607 (autocmd breaks snippets)
-          -- vim.api.nvim_create_autocmd('CompleteChanged', {
-          --   buffer = event.buf,
-          --   callback = function()
-          --     local info = vim.fn.complete_info({ 'selected' })
-          --     local completionItem = vim.tbl_get(vim.v.completed_item, 'user_data', 'nvim', 'lsp', 'completion_item')
-          --     if not completionItem then
-          --       return
-          --     end
-          --
-          --     local resolvedItem = vim.lsp.buf_request_sync(
-          --       event.buf,
-          --       vim.lsp.protocol.Methods.completionItem_resolve,
-          --       completionItem,
-          --       500
-          --     ) or {}
-          --
-          --     local docs = vim.tbl_get(resolvedItem[event.data.client_id], 'result', 'documentation', 'value') or ''
-          --
-          --     local winData = vim.api.nvim__complete_set(info['selected'], { info = docs })
-          --     if not winData.winid or not vim.api.nvim_win_is_valid(winData.winid) then
-          --       return
-          --     end
-          --
-          --     if docs == '' then
-          --       vim.api.nvim_set_option_value('winhighlight', 'NormalFloat:Float', { win = winData.winid })
-          --       return
-          --     end
-          --
-          --     vim.api.nvim_set_option_value('winhighlight', 'Normal:NormalFloat', { win = winData.winid })
-          --     vim.treesitter.start(winData.bufnr, 'markdown')
-          --     vim.wo[winData.winid].conceallevel = 3
-          --   end,
-          -- })
+          vim.api.nvim_create_autocmd({ 'InsertEnter' }, {
+            buffer = event.buf,
+            callback = function()
+              vim.lsp.completion.trigger()
+            end,
+          })
+          -- based on https://github.com/neovim/neovim/issues/29225#issuecomment-2159428607 (autocmd there breaks snippets)
+          vim.api.nvim_create_autocmd('CompleteChanged', {
+            buffer = event.buf,
+            callback = function()
+              local info = vim.fn.complete_info({ 'selected' })
+              local completionItem = vim.tbl_get(vim.v.completed_item, 'user_data', 'nvim', 'lsp', 'completion_item')
+              if not completionItem then
+                return
+              end
+
+              local resolvedItem = vim.lsp.buf_request_sync(
+                event.buf,
+                vim.lsp.protocol.Methods.completionItem_resolve,
+                completionItem,
+                500
+              ) or {}
+
+              local docs = vim.tbl_get(resolvedItem[event.data.client_id], 'result', 'documentation', 'value') or ''
+
+              local winData = vim.api.nvim__complete_set(info['selected'], { info = docs })
+              if not winData.winid or not vim.api.nvim_win_is_valid(winData.winid) then
+                return
+              end
+
+              if docs == '' then
+                vim.api.nvim_set_option_value('winhighlight', 'NormalFloat:Float', { win = winData.winid })
+                return
+              end
+
+              vim.api.nvim_set_option_value('winhighlight', 'Normal:NormalFloat', { win = winData.winid })
+              vim.treesitter.start(winData.bufnr, 'markdown')
+              vim.wo[winData.winid].conceallevel = 3
+            end,
+          })
           vim.lsp.completion.enable(true, event.data.client_id, event.buf, {
             autotrigger = true,
             convert = function(item)
@@ -142,7 +142,7 @@ return {
 
           vim.o.completeitemalign = 'abbr,menu,kind'
           vim.o.pumheight = 6
-          vim.opt.completeopt = { 'menuone', 'noselect', 'noinsert', 'fuzzy', 'popup' }
+          vim.opt.completeopt = { 'menuone', 'noinsert', 'fuzzy', 'popup' }
 
           -- vim.keymap.set('s', '<C-l>', function()
           --   if vim.snippet.active({ direction = 1 }) then
@@ -205,55 +205,56 @@ return {
           vim.keymap.set('n', ']d', next_diag, { desc = 'LSP: Go to next [D]iagnostic message', buffer = event.buf })
           vim.keymap.set('n', '[d', prev_diag, { desc = 'LSP: Go to prev [D]iagnostic message', buffer = event.buf })
 
-          -- Move to next/prev reference IN THE BUFFER
-          -- NOTE: it's fast, no need of cache apparently
-          ---@param direction 1 | -1
-          local function move_reference(direction)
-            vim.lsp.buf.references(nil, {
-              on_list = function(options)
-                local current_buffer = vim.api.nvim_buf_get_name(0)
-                local current_position = vim.api.nvim_win_get_cursor(0)
-                current_position[2] = current_position[2] + 1
-                local items = vim
-                  .iter(options.items)
-                  :filter(function(item)
-                    return item.filename == current_buffer
-                  end)
-                  :totable()
-                if #items == 0 then
-                  vim.notify('No references in the buffer', vim.log.levels.INFO)
-                  return
-                end
-                local next_reference
-                if direction == 1 then
-                  next_reference = vim.iter(items):find(function(item)
-                    return (item.lnum == current_position[1] and item.col > current_position[2])
-                      or (item.lnum > current_position[1])
-                  end)
-                  if not next_reference then
-                    next_reference = items[1]
-                  end
-                else
-                  next_reference = vim.iter(options.items):rfind(function(item)
-                    return (item.lnum == current_position[1] and item.col < current_position[2])
-                      or (item.lnum < current_position[1])
-                  end)
-                  if not next_reference then
-                    next_reference = items[#items]
-                  end
-                end
-                vim.api.nvim_win_set_cursor(0, {
-                  next_reference.lnum,
-                  next_reference.col - 1,
-                })
-              end,
-            })
+          -- replaced my own implementation with Folke's which is more performant: https://github.com/folke/snacks.nvim/blob/main/lua/snacks/words.lua#L117
+          local ns = vim.api.nvim_create_namespace('vim_lsp_references')
+
+          ---@alias LspWord {from:{[1]:number, [2]:number}, to:{[1]:number, [2]:number}} 1-0 indexed
+
+          ---@return LspWord[] words, number? current
+          local function get_lsp_word()
+            local cursor = vim.api.nvim_win_get_cursor(0)
+            local current, ret = nil, {} ---@type number?, LspWord[]
+            for _, extmark in ipairs(vim.api.nvim_buf_get_extmarks(0, ns, 0, -1, { details = true })) do
+              local w = {
+                from = { extmark[2] + 1, extmark[3] },
+                to = { extmark[4].end_row + 1, extmark[4].end_col },
+              }
+              ret[#ret + 1] = w
+              if
+                cursor[1] >= w.from[1]
+                and cursor[1] <= w.to[1]
+                and cursor[2] >= w.from[2]
+                and cursor[2] <= w.to[2]
+              then
+                current = #ret
+              end
+            end
+            return ret, current
           end
+
+          ---@param count number
+          ---@param direction 1 | -1
+          ---@param cycle? boolean
+          local function move_reference(count, direction, cycle)
+            local words, idx = get_lsp_word()
+            if not idx then
+              return
+            end
+            idx = idx + count * direction
+            if cycle then
+              idx = (idx - 1) % #words + 1
+            end
+            local target = words[idx]
+            if target then
+              vim.api.nvim_win_set_cursor(0, target.from)
+            end
+          end
+
           vim.keymap.set('n', ']r', function()
-            move_reference(1)
+            move_reference(vim.v.count1, 1, true)
           end, { desc = 'LSP: Go to next [R]eference', buffer = event.buf })
           vim.keymap.set('n', '[r', function()
-            move_reference(-1)
+            move_reference(vim.v.count1, -1, true)
           end, { desc = 'LSP: Go to previous [R]eference', buffer = event.buf })
 
           vim.keymap.set('n', 'gr', '<NOP>', { desc = 'LSP mappings', buffer = event.buf })
@@ -297,25 +298,21 @@ return {
             })
           end
 
-          -- can't get float_winnr
-          -- local float_winnr = -1
-          -- vim.keymap.set('i', '<C-b>', function()
-          --   if vim.api.nvim_win_is_valid(float_winnr) then
-          --     local cursor_pos = vim.api.nvim_win_get_cursor(float_winnr)
-          --     local new_row = math.max(1, cursor_pos[1] - vim.o.scrolloff / 2)
-          --     vim.api.nvim_win_set_cursor(float_winnr, { new_row, cursor_pos[2] })
-          --   else
-          --     return '<C-b>'
-          --   end
-          -- end, { desc = 'Scroll Backwards Docs' })
-          -- vim.keymap.set('i', '<C-f>', function()
-          --   if vim.api.nvim_win_is_valid(float_winnr) then
-          --     local cursor_pos = vim.api.nvim_win_get_cursor(float_winnr)
-          --     pcall(vim.api.nvim_win_set_cursor, float_winnr, { cursor_pos[1] + vim.o.scrolloff / 2, cursor_pos[2] })
-          --   else
-          --     return '<C-f>'
-          --   end
-          -- end, { desc = 'Scroll Forwards Docs' })
+          for _, key in ipairs({ '<C-b>', '<C-f>' }) do
+            vim.keymap.set({ 'n', 'i' }, key, function()
+              local winnr = vim.b.lsp_floating_preview
+              if not winnr or not vim.api.nvim_win_is_valid(winnr) then
+                local keys = vim.api.nvim_replace_termcodes(key, true, false, true)
+                vim.api.nvim_feedkeys(keys, 'n', true)
+                return
+              end
+              local cursor_pos = vim.api.nvim_win_get_cursor(winnr)
+              local win_config = vim.api.nvim_win_get_config(winnr)
+              local new_row = key == '<C-b>' and math.max(cursor_pos[1] - win_config.height + 1, 1)
+                or math.min(cursor_pos[1] + win_config.height - 1, vim.fn.line('$', winnr))
+              vim.api.nvim_win_set_cursor(winnr, { new_row, cursor_pos[2] })
+            end, { desc = 'Scroll Docs' })
+          end
 
           -- based on https://github.com/mfussenegger/nvim-qwahl/blob/main/lua/qwahl.lua#L446C1-L468C4
           ---@param bufnr? integer 0 for current buffer; nil for all diagnostic
@@ -329,7 +326,7 @@ return {
                 if new_line then
                   d.message = d.message:sub(1, new_line - 1)
                 end
-                local signs = { ' ', ' ', '', ' ' }
+                local signs = { ' ', ' ', ' ', ' ' }
                 local bufname = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(d.bufnr), ':p:.')
                 local lnum = d.lnum or d.end_lnum
                 return string.format('%s%s (%s:%s)', signs[d.severity], d.message, bufname, lnum + 1)
@@ -353,24 +350,31 @@ return {
             wd.populate_workspace_diagnostics(client, event.buf)
           end
 
+          -- if client:supports_method('textDocument/foldingRange') then
+          --   vim.wo.foldmethod = 'expr'
+          --   vim.wo.foldexpr = 'v:lua.vim.lsp.foldexpr()'
+          -- end
+
           if client.server_capabilities.documentHighlightProvider then
             local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
             -- Highlight references of the word under your cursor
-            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI', 'ModeChanged' }, {
               group = highlight_augroup,
               buffer = event.buf,
               callback = function()
-                vim.lsp.buf.clear_references()
-                vim.lsp.buf.document_highlight()
+                vim.schedule(function()
+                  vim.lsp.buf.clear_references()
+                  vim.lsp.buf.document_highlight()
+                end)
               end,
             })
 
-            -- When you move your cursor, the highlights will be cleared
-            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-              group = highlight_augroup,
-              buffer = event.buf,
-              callback = vim.lsp.buf.clear_references,
-            })
+            -- -- When you move your cursor, the highlights will be cleared
+            -- vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+            --   group = highlight_augroup,
+            --   buffer = event.buf,
+            --   callback = vim.lsp.buf.clear_references,
+            -- })
 
             -- Clear highlight when detaching lsp (fix some lsp errors)
             vim.api.nvim_create_autocmd('LspDetach', {
@@ -385,17 +389,23 @@ return {
         end,
       })
 
-      -- thanks Maria: https://github.com/MariaSolOs/dotfiles/blob/main/.config/nvim/lua/lsp.lua#L144C1-L156C2
-      -- Override the virtual text diagnostic handler so that the most severe diagnostic is shown first.
+      -- thanks lewis: https://github.com/lewis6991/dotfiles/blob/main/config/nvim/lua/lewis6991/diagnostic.lua#L25
+      -- Override the virtual text diagnostic handler so that the most severe diagnostic is shown first,
+      -- without changing normal order for same severity.
       local show_handler = vim.diagnostic.handlers.virtual_text.show
       assert(show_handler)
       local hide_handler = vim.diagnostic.handlers.virtual_text.hide
       vim.diagnostic.handlers.virtual_text = {
         show = function(ns, bufnr, diagnostics, opts)
-          table.sort(diagnostics, function(diag1, diag2)
-            return diag1.severity > diag2.severity
-          end)
-          return show_handler(ns, bufnr, diagnostics, opts)
+          local max_severity_per_line = {}
+          for _, d in pairs(diagnostics) do
+            local m = max_severity_per_line[d.lnum]
+            if not m or d.severity < m.severity then
+              max_severity_per_line[d.lnum] = d
+            end
+          end
+          local filtered_diagnostics = vim.tbl_values(max_severity_per_line)
+          show_handler(ns, bufnr, filtered_diagnostics, opts)
         end,
         hide = hide_handler,
       }
