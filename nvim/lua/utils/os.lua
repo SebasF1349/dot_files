e3c5f153c396975403045fc8bdc4447d6b6ce3cb
SebasF1349 <ratablanca711@gmail.com>
2025-02-01 10:01:19 -0300

    refactor(nv): os.correct_separator

--- nvim/lua/utils/os.lua
+++ nvim/lua/utils/os.lua
@@ -2,14 +2,18 @@ local M = {}
 
 M.is_win = vim.fn.has('win32') ~= 0
 
-M.dir_separator = vim.fn.has('win32') ~= 0 and '\\' or '/'
+M.dir_separator = M.is_win and '\\' or '/'
 
-M.joinpath = function(...)
-  local path = vim.fs.joinpath(...)
+M.correct_separator = function(path)
   if M.is_win then
     path = path:gsub('/', '\\')
   end
   return path
+end
+
+M.joinpath = function(...)
+  local path = vim.fs.joinpath(...)
+  return M.correct_separator(path)
 end
 
 return M
