2d643dccfa74bea3624f8213ca6de67bb25f247b
SebasF1349 <ratablanca711@gmail.com>
2025-03-29 19:51:07 -0300

    feat(nv): more ftplugins: fugitiveblame & gitcommit

---
+++ nvim/ftplugin/fugitiveblame.lua
@@ -0,0 +1,19 @@
+vim.wo.statuscolumn = ' '
+vim.wo.signcolumn = 'yes'
+vim.bo.buflisted = false
+vim.bo.bufhidden = 'wipe'
+
+vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = 0, silent = true })
+
+vim.keymap.set(
+  'n',
+  'dt',
+  ':Gtabedit <Plug><cfile><Bar>Gdiffsplit<CR>',
+  { desc = 'Open [D]iff in New [T]ab', remap = true, buffer = 0 }
+)
+
+vim.b.undo_ftplugin = (vim.b.undo_ftplugin or '')
+  .. '\n '
+  .. 'setlocal statuscolumn< signcolumn< buflisted< bufhidden<'
+  .. ' | sil! nunmap <buffer> dt'
+  .. ' | sil! nunmap <buffer> q'
---
+++ nvim/ftplugin/gitcommit.lua
@@ -0,0 +1,12 @@
+vim.wo.spell = true
+vim.bo.spelllang = 'es,en'
+
+local line = vim.api.nvim_get_current_line()
+if #line == 0 then
+  vim.cmd.startinsert()
+end
+
+vim.b.undo_ftplugin = (vim.b.undo_ftplugin or '')
+  .. '\n '
+  .. 'setlocal spell< spelllang< '
+  .. ' | sil! nunmap <buffer> <leader>x'
