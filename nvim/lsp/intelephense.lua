bd0fb81855e138bac70ed17ec86318b06d99471f
SebasF1349 <ratablanca711@gmail.com>
2025-03-30 08:28:41 -0300

    test(nv): add some intelephense setting I found out as recommended
    I have no idea if it works or if it's useful

--- nvim/lsp/intelephense.lua
+++ nvim/lsp/intelephense.lua
@@ -2,6 +2,14 @@ return {
   cmd = { 'intelephense', '--stdio' },
   filetypes = { 'php' },
   root_markers = { 'composer.root', 'composer.json', '.git' },
+  commands = {
+    IntelephenseIndex = {
+      function()
+        local clients = vim.lsp.get_clients({ name = 'intelephense' })
+        clients[1]:exec_cmd({ title = 'php_index_workspace', command = 'intelephense.index.workspace' })
+      end,
+    },
+  },
   settings = {
     intelephense = {
       format = {
