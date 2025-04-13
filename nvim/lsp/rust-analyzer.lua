bc57e312d4f9abca9f0ac94fd7a5cefbd881a96c
SebasF1349 <ratablanca711@gmail.com>
2025-04-13 09:16:42 -0300

    feat(nv): add CargoReload rust-analyzer cmd

--- nvim/lsp/rust-analyzer.lua
+++ nvim/lsp/rust-analyzer.lua
@@ -1,6 +1,21 @@
+local function reload_workspace(bufnr)
+  bufnr = vim.validate('bufnr', bufnr, 'number')
+  local clients = vim.lsp.get_clients({ bufnr = bufnr, name = 'rust_analyzer' })
+  for _, client in ipairs(clients) do
+    vim.notify('Reloading Cargo Workspace')
+    client:request('rust-analyzer/reloadWorkspace', nil, function(err)
+      if err then
+        error(tostring(err))
+      end
+      vim.notify('Cargo workspace reloaded')
+    end, 0)
+  end
+end
+
 return {
   cmd = { 'rust-analyzer' },
   filetypes = { 'rust' },
+  -- check https://github.com/neovim/nvim-lspconfig/blob/master/lsp/rust_analyzer.lua
   root_markers = { 'Cargo.toml' },
   capabilities = {
     experimental = {
@@ -12,6 +27,11 @@ return {
     if config.settings and config.settings['rust-analyzer'] then
       init_params.initializationOptions = config.settings['rust-analyzer']
     end
+  end,
+  on_attach = function()
+    vim.api.nvim_buf_create_user_command(0, 'CargoReload', function()
+      reload_workspace(0)
+    end, { desc = 'Reload current cargo workspace' })
   end,
   settings = {
     autoformat = true,
