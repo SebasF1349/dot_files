e88e88ded4d641705d046f8b0b1c144e4736466f
SebasF1349 <ratablanca711@gmail.com>
2025-04-15 21:09:35 -0300

    feat(nv): add git log in visual mode

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -1,6 +1,12 @@
 vim.keymap.set('n', '<leader>gg', '<cmd>tab G<CR>]]', { desc = 'Open fu[G]itive in a new tab', remap = true })
 vim.keymap.set('n', '<leader>gd', '<cmd>Gvdiffsplit<CR>', { desc = '[D]iff Current File' })
 vim.keymap.set('n', '<leader>gl', '<cmd>tab Git log -50 --oneline<cr>', { desc = 'fu[G]itive [L]og' })
+vim.keymap.set(
+  'x',
+  '<leader>gl',
+  ":<C-u>execute 'Git log -L ' . line(\"'<\") . ',' . line(\"'>\") . ':%'<CR>",
+  { desc = 'fu[G]itive [L]og' }
+)
 vim.keymap.set('n', '<leader>gL', '<cmd>tab Git log -50 --oneline %<cr>', { desc = 'fu[G]itive [L]og File' })
 
 return {
