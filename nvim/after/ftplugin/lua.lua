05fb702e05abc60276e20f39435a0b5a014a7e5c
SebasF1349 <ratablanca711@gmail.com>
2025-04-27 15:00:30 -0300

    feat(nv): use builtin help in lua files

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -1,5 +1,8 @@
 vim.b.contextStatus = { 'function_declaration' }
 
+vim.bo.keywordprg = ':help'
+vim.bo.omnifunc = 'v:lua.vim.lua_omnifunc'
+
 local snippets = {
   fn = [[local function ${1:FunctionName}(${2:})
 {
@@ -12,4 +15,7 @@ for key, snippet in pairs(snippets) do
   _G.addSnippet(key, snippet)
 end
 
-vim.b.undo_ftplugin = (vim.b.undo_ftplugin or '') .. '\n ' .. 'unlet! b:contextStatus'
+vim.b.undo_ftplugin = (vim.b.undo_ftplugin or '')
+  .. '\n '
+  .. 'unlet! b:contextStatus'
+  .. ' | setlocal keywordprg< omnifunc<'
