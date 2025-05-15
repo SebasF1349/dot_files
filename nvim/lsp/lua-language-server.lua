cf792864518cb1c7f00de699e894700b7191f53f
SebasF1349 <ratablanca711@gmail.com>
2025-05-15 19:16:48 -0300

    refactor(nv): just simplify lua-language-server settings
    apparently settings are not set if there is a .luarc file anyway, luals
    is weird

--- nvim/lsp/lua-language-server.lua
+++ nvim/lsp/lua-language-server.lua
@@ -7,20 +7,16 @@ return {
     '.luacheckrc',
     '.stylua.toml',
     'stylua.toml',
-    'selene.toml',
-    'selene.yml',
     '.git',
   },
   log_level = vim.lsp.protocol.MessageType.Warning,
-  on_init = function(client)
-    local path = client.workspace_folders[1].name
-    if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
-      return
-    end
-
-    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+  settings = {
+    Lua = {
+      format = { enable = false },
+      completion = { callSnippet = 'Replace' },
+      hint = { enable = true, arrayIndex = 'Disable' },
+      telemetry = { enable = false },
       runtime = { version = 'LuaJIT' },
-      -- Make the server aware of Neovim runtime files
       workspace = {
         checkThirdParty = false,
         library = {
@@ -28,14 +24,6 @@ return {
           vim.fn.stdpath('data') .. '/lazy', -- plugins types
         },
       },
-    })
-  end,
-  settings = {
-    Lua = {
-      format = { enable = false },
-      completion = { callSnippet = 'Replace' },
-      hint = { enable = true, arrayIndex = 'Disable' },
-      telemetry = { enable = false },
     },
   },
 }
