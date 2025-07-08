97d2b3568e0496ea317c0cfe2dfc3c2961743aae
SebasF1349 <ratablanca711@gmail.com>
2025-07-08 17:58:25 -0300

    clean(nv): remove evaluate operator that never needed

--- nvim/lua/core/operators.lua
+++ nvim/lua/core/operators.lua
@@ -6,72 +6,12 @@ function _G.opfunc(func_name)
 end
 
 --------------------------------------------------
--- Evaluate
---------------------------------------------------
-
--- shameless stealing from mini.operators: https://github.com/echasnovski/mini.nvim/blob/main/lua/mini/operators.lua#L323
-local function inspect_objects(...)
-  local objects = {}
-  -- Not using `{...}` because it removes `nil` input
-  for i = 1, select('#', ...) do
-    local v = select(i, ...)
-    table.insert(objects, vim.inspect(v))
-  end
-
-  return vim.split(table.concat(objects, '\n'), '\n')
-end
-
-local function eval_lua_lines(lines)
-  -- Copy to not modify input
-  local lines_copy, n = vim.deepcopy(lines), #lines
-  lines_copy[n] = (lines_copy[n]:find('^%s*return%s+') == nil and 'return ' or '') .. lines_copy[n]
-
-  local str_to_eval = table.concat(lines_copy, '\n')
-
-  -- Allow returning tuple with any value(s) being `nil`
-  return inspect_objects(assert(loadstring(str_to_eval))())
-end
-
-function _G.Evaluate(mode)
-  local starting = vim.api.nvim_buf_get_mark(0, '[')
-  local ending = vim.api.nvim_buf_get_mark(0, ']')
-  local lines = {}
-  if mode == 'char' then
-    lines = vim.api.nvim_buf_get_text(0, starting[1] - 1, starting[2], ending[1] - 1, ending[2] + 1, {})
-  elseif mode == 'line' then
-    local len_last_line = #vim.fn.getline(ending[1])
-    lines = vim.api.nvim_buf_get_text(0, starting[1] - 1, 0, ending[1] - 1, len_last_line, {})
-  elseif mode == 'block' then
-    for i = starting[1] - 1, ending[1] - 1 do
-      table.insert(lines, vim.api.nvim_buf_get_text(0, i, starting[2], i, ending[2] + 1, {})[1])
-    end
-  end
-  local text_to_replace = eval_lua_lines(lines)
-  if mode == 'char' then
-    vim.api.nvim_buf_set_text(0, starting[1] - 1, starting[2], ending[1] - 1, ending[2] + 1, text_to_replace)
-  elseif mode == 'line' then
-    vim.api.nvim_buf_set_lines(0, starting[1] - 1, ending[1], true, text_to_replace)
-  elseif mode == 'block' then
-    for i = starting[1] - 1, ending[1] - 1 do
-      vim.api.nvim_buf_set_text(0, i, starting[2], i, ending[2] + 1, text_to_replace)
-    end
-  end
-end
-
-vim.keymap.set(
-  { 'n', 'v' },
-  'g=',
-  _G.opfunc('_G.Evaluate'),
-  { desc = 'Evaluate Expression', silent = true, expr = true }
-)
-
---------------------------------------------------
 -- Surround
 --------------------------------------------------
 
 -- TODO: add 'surround with function' with fn.input to input the function name
+-- TODO: try to reduce boilerplate
 
--- FIX: try to reduce boilerplate
 local surround = {
   ['('] = { { '(' }, { ')' } },
   ['b'] = { { '(' }, { ')' } },
