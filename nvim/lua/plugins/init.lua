54743f8b97dd4a513235ebb40b1902c886ca8e8a
SebasF1349 <ratablanca711@gmail.com>
2025-07-21 17:10:59 -0300

    clean(nv): limpieza de plugins
    - cloak
    - tailwind-sorter
    - guess-indent
    - crates

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,33 +1,10 @@
 return {
-  { 'NMAC427/guess-indent.nvim', event = 'InsertEnter', opts = {} },
-
   {
     'danymat/neogen',
     keys = {
       { 'gd', "<cmd>lua require('neogen').generate()<CR>", desc = 'Add [D]ocs' },
     },
     opts = { snippet_engine = 'nvim' },
-  },
-
-  {
-    'saecki/crates.nvim',
-    event = { 'BufRead Cargo.toml' },
-    config = function()
-      local crates = require('crates')
-      crates.setup({
-        completion = {
-          cmp = { enabled = true },
-        },
-      })
-      crates.show()
-      vim.keymap.set('n', 'K', function()
-        if vim.fn.expand('%:t') == 'Cargo.toml' and require('crates').popup_available() then
-          require('crates').show_popup()
-        else
-          vim.lsp.buf.hover()
-        end
-      end, { desc = 'Show Crate Documentation', buffer = true })
-    end,
   },
 
   { 'artemave/workspace-diagnostics.nvim' },
