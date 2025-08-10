defe2fb3a934e092b9de49b513195543d4d0b1cb
SebasF1349 <ratablanca711@gmail.com>
2025-08-09 22:40:14 -0300

    feat(nv): nicer tabs

---
+++ nvim/plugin/tab.lua
@@ -0,0 +1,38 @@
+-- https://github.com/TheLeoP/nvim-config/blob/master/plugin/tab.lua
+local api = vim.api
+
+function _G._personal_tab()
+  local last = vim.fn.tabpagenr('$')
+  local current = vim.fn.tabpagenr()
+
+  local out = {} ---@type string[]
+  for i = 1, last do
+    table.insert(out, i == current and '%#TabLineSel#' or '%#TabLine#')
+
+    table.insert(out, (' %d: %%{v:lua._personal_tab_label(%d)} '):format(i, i))
+  end
+
+  return table.concat(out)
+end
+
+---@param i integer
+function _G._personal_tab_label(i)
+  local buflist = vim.fn.tabpagebuflist(i) ---@type integer[]
+  local winnr = vim.fn.tabpagewinnr(i)
+  local buf = buflist[winnr]
+  local name = api.nvim_buf_get_name(buf)
+  local protocol = name:match('^(.*)://')
+  if name == '' then
+    return '[No name]'
+  elseif protocol == 'fugitive' or protocol == 'health' then
+    return protocol .. '://'
+  elseif vim.endswith(name, '/') or vim.endswith(name, '\\') then
+    local dirname = name:sub(1, -2)
+    local tail = vim.fn.fnamemodify(dirname, ':t')
+    return tail .. '/'
+  end
+  local tail = vim.fn.fnamemodify(name, ':t')
+  return tail
+end
+
+vim.o.tabline = '%{%v:lua._personal_tab()%}'
