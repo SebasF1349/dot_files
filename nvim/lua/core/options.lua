da8e54a323c495213197a9363d0e7db67862cf69
SebasF1349 <ratablanca711@gmail.com>
2025-09-12 22:42:27 -0300

    clean(nv): reorder diffopt for better comparation
    - new defaults have been added, but algorithm:histogram is a must

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -59,7 +59,7 @@ vim.opt.signcolumn = 'yes'
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
-vim.o.diffopt = 'internal,filler,closeoff,indent-heuristic,linematch:60,algorithm:histogram,inline:char'
+vim.o.diffopt = 'internal,filler,closeoff,indent-heuristic,inline:char,linematch:60,algorithm:histogram'
 
 vim.opt.fileencoding = 'utf-8'
 
