b2a3fbb75316a4c1b42ac907ca7e2e488bcc1842
SebasF <33849340+SebasF1349@users.noreply.github.com>
2026-01-19 20:19:49 -0300

    clean(wez): clean is_nvim function

--- wezterm/utils.lua
+++ wezterm/utils.lua
@@ -6,13 +6,6 @@ local M = {}
 ---@return boolean
 function M.is_nvim(pane)
 	return pane:get_user_vars().IS_NVIM == "true"
-		or (pane:get_foreground_process_name() and (pane:get_foreground_process_name():find("nv") or pane
-			:get_foreground_process_name()
-			:find("n?vim")))
-		or (
-			pane:get_user_vars().WEZTERM_PROG
-			and (pane:get_user_vars().WEZTERM_PROG:find("nv") or pane:get_user_vars().WEZTERM_PROG:find("n?vim"))
-		)
 end
 
 function M.is_windows()
