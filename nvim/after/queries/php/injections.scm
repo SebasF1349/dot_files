a06452c296c46c0a9f3800bdfa8ac34c313b3ccb
Sebas <fellsebastian@yahoo.com.ar>
2026-04-30 19:23:13 -0300

    feat(nv): add js injections for registerJs
    - needs more testing
    - add registerCss later on if it works

--- nvim/after/queries/php/injections.scm
+++ nvim/after/queries/php/injections.scm
@@ -4,3 +4,12 @@
     (_
         (string_content) @injection.content))
 (#set! injection.language "html"))
+
+((member_call_expression
+    name: (name) @method_name
+    arguments: (arguments
+      (argument
+        (_
+          (string_content) @injection.content))))
+  (#eq? @method_name "registerJs")
+  (#set! injection.language "javascript"))
