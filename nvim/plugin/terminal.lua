84321cf51377ce9bdcf131237cf61982dfbd4b61
Sebas <fellsebastian@yahoo.com.ar>
2026-07-26 13:07:58 -0300

    clean(nv): no need to set options for pwsh anymore

--- nvim/plugin/terminal.lua
+++ nvim/plugin/terminal.lua
@@ -19,14 +19,6 @@ local terminal_autocmds = api.nvim_create_augroup('Terminal Autocmds', { clear =
 
 if require('utils.os').is_win then
   vim.o.shell = 'pwsh'
-  vim.o.shellcmdflag =
-    '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
-  -- vim.o.shellcmdflag =
-  --   "-NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';$PSStyle.OutputRendering=''plaintext'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
-  vim.o.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-  vim.o.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-  vim.o.shellquote = ''
-  vim.o.shellxquote = ''
 else
   vim.o.shell = '/bin/bash'
 end
