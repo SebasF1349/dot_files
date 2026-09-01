3a676663bf16422775253a16adcac2654360c66b
Sebas <fellsebastian@yahoo.com.ar>
2026-09-01 13:29:56 -0300

    fix(rofi): more performant find-files picker

--- rofi/pickers/find-files.sh
+++ rofi/pickers/find-files.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
 DIR="${SEARCH_DIR:-$HOME}"
+CACHE_FILE="/tmp/rofi_fd_cache"
 
-if [ -z "$1" ]; then
-    fdfind -t f . "$DIR" | awk -F/ '{printf "%s\0info\x1f%s\n", $NF, $0}'
-else
-    xdg-open "$ROFI_INFO" > /dev/null 2>&1 &
+generate_cache() {
+    fdfind -t f . "$DIR" 2>/dev/null | awk -F/ '{printf "%s\0info\x1f%s\n", $NF, $0}' >"$CACHE_FILE.tmp" && mv "$CACHE_FILE.tmp" "$CACHE_FILE"
+}
+
+if [ ! -f "$CACHE_FILE" ]; then
+    generate_cache
+elif [ $(($(date +%s) - $(stat -c %Y "$CACHE_FILE"))) -gt 3600 ]; then
+    generate_cache &
+fi
+
+if [ -n "$ROFI_INFO" ]; then
+    xdg-open "$ROFI_INFO" >/dev/null 2>&1 &
     exit 0
 fi
+
+cat "$CACHE_FILE"
